8d98a1d04671533f69824196181861090c8f20db
AbhijitPrajapati <talk.abhijitp@gmail.com>
2024-05-17 21:44:52 -0400

    started lstm

--- main.py
+++ main.py
@@ -1,185 +1,120 @@
 import numpy as np
-from autograd import elementwise_grad
-
-
-
-# function: data handler
-        
-    
 
 class Layer:
-    def __init__(self, activation, num_neurons, num_previous_neurons):
-        # initialize weights
-        weights = np.random.normal(0, 2 / num_previous_neurons, num_neurons * num_previous_neurons)
 
-        # weight structure: 
-        # each row corresponds to the weights of a neuron
-        # each column corresponds to the weights of neurons connected to a neuron in the previous layer
-        self.weights = np.reshape(weights, (num_neurons, num_previous_neurons))
+    # input_size: the size of the input of one timeframe
+    # num_frames: the number of time frames
+    def __init__(self, input_size, num_frames):
+        self.num_frames = num_frames
 
-        # initialize biases as 0, one for each neuron
-        self.biases = np.zeros((num_neurons, 1))
 
-        # initialize gradients of each weight as 0
-        self.gradients = np.zeros((num_neurons, num_previous_neurons))
-
-        self.activation = activation
-
-        self.activation_derivatives = np.zeros((num_neurons, 1))
-
-        self.neuron_grads = np.zeros((num_neurons, 1))
-
-        self.inputs = np.zeros((num_neurons, 1))
-
+        # intializes weights in shape using orthogonal initialization
+        # (H.E initialization for now)
+        self.weights = {
+            # weights for using the inputs to determine what percent of the cell state should be remembered
+            'forget': np.random.normal(0, 0.1, (num_frames, input_size)),
+            # weights for using the hidden state to determine what percent of the cell state should be remembered
+            'forget_recurrent': np.random.normal(0, 0.1, (num_frames, num_frames)),
+            # weights for using the inputs to determine the candidate cell state
+            'candidate_cell_state':  np.random.normal(0, 0.1, (num_frames, input_size)),
+            # weights for using the hidden state to determine the candidate cell state
+            'candidate_cell_state_recurrent': np.random.normal(0, 0.1, (num_frames, num_frames)),
+            # weights for using the input to determine what percent of the candidate cell state should be added
+            'cell_state':  np.random.normal(0, 0.1, (num_frames, input_size)),
+            # weights for using the hidden state to determine what percent of the candidate cell state should be added
+            'cell_state_recurrent': np.random.normal(0, 0.1, (num_frames, num_frames)),
+            # weights for using the input to detemine the new short term memory
+            'output':  np.random.normal(0, 0.1, (num_frames, input_size)),
+            # weights for using the input to detemine the new short term memory
+            'output_recurrent': np.random.normal(0, 0.1, (num_frames, num_frames))
+        }
         
-        # data needed for backpropagation
+        self.biases = {
+            # bias for forget gate
+            'forget': np.zeros((num_frames)),
+            # biases for cell state computation
+            'candidate_cell_state': np.zeros((num_frames)),
+            'cell_state': np.zeros((num_frames)),
+            # bias for output
+            'output': np.zeros((num_frames))
+        }
+
+
+        self.cell_state = np.zeros((num_frames))
+        
+        self.hidden_state = np.zeros((num_frames))
+
+        # shape will be (num_frames, num_frames) because the different rows keep track of the hidden states of each of the units
+        self.outputs = []
+
+    # input: vector
+    def sigmoid(input):
+        return [1 / (1 + np.exp(-x)) for x in input]
     
+    # input: vector
+    def tanh(input):
+        return [  (np.exp(x) - np(-x)) / (np.exp(x) + np.exp(-x))  for x in input]
+
+
+    def forget_gate(self, input):
+        # get the percentage of the cell state to be remembered
+        input_forget = np.dot( self.weights['forget'], input )
+        hidden_forget = np.dot( self.weights['forget_recurrent'], self.hidden_state )
+        forget = input_forget + hidden_forget + self.biases['forget']
+        self.cell_state *= forget
+    
+    def input_gate(self, input):
+         # get a potential cell state
+        input_candidate = np.dot( self.weights['candidate_cell_state'], input)
+        hidden_candidate = np.dot( self.weights['candidate_cell_state_recurrent'], self.hidden_state )
+        candidate = input_candidate + hidden_candidate + self.biases['candidate_cell_state']
+        candidate = self.tanh(candidate)
+
+        # get the percentage of the potential cell state to be added to the cell state
+        input_cell_state = np.dot( self.weights['cell_state'], input )
+        hidden_cell_state = np.dot( self.weights['cell_state_recurrent'], self.hidden_state )
+        percent = input_cell_state + hidden_cell_state + self.biases['cell_state']
+        percent = self.sigmoid(percent)
+
+        self.cell_state += (candidate * percent)
+
+    def output_gate(self, inputs):
+        # get percentage of short term memory to make as new short term memory
+        input_output = np.dot( self.weights['output'], inputs)
+        hidden_output = np.dot( self.weights['output_recurrent'], self.hidden_state )
+        percent = input_output + hidden_output + self.biases['output']
+        percent = self.sigmoid(percent)
+
+        # get potential output
+        potential = self.tanh(self.cell_state)
+
+        # update
+        self.hidden_state = potential * percent
+        self.outputs.append(self.hidden_state)
+
+
+
+    # inputs shape: (num features, num time frames)
     def __call__(self, inputs):
-        self.inputs = np.array(inputs)
+        for input in inputs:
 
-        # multiply the weights that connect to each previous neuron with the neurons value
-        weighted_sum = np.dot(self.weights, inputs) + self.biases
+            # forget gate
+            self.forget_gate(input)
 
-        self.preactivations = weighted_sum
+            # input gate
+            self.input_gate(input)
 
-        # store the derivatives of the activation function with respect to the weighted sum
-        # weighted sum is in n, 1 shape
-        self.activation_derivatives = elementwise_grad(self.activation)(weighted_sum)
-
-        # run outputs through activation function
-        # weighted sum is in n, 1 shape
-        activations = self.activation(weighted_sum)
-
-    
-        return activations
-    
-
-
-# class: Optimizer ??
-
-class MultilayerPerceptron:
-    # layer sizes: sizes of the non-input layers
-    # activations: activation fucntions for non-input layers
-    def __init__(self, num_inputs, loss_function):
-        # non-input layers
-        self.layers = []
-        self.loss_function = loss_function
+            # output gate
+            self.output_gate(input)
         
-        self.num_inputs = num_inputs
 
-    def add_layer(self, activation, num_neurons):
-        previous_layer_size = self.num_inputs if len(self.layers) == 0 else len(self.layers[-1].weights)
-        self.layers.append(Layer(activation, num_neurons, previous_layer_size))
+        # output should be of shape (batch size, hidden dim)
 
-    def __call__(self, inputs):
-        # manually perfmorm forward pass for input layer
-        # next_inputs = []
-        # for n in self.layers[0].neurons:
-        #     next_inputs.append(n(inputs))
-        next_inputs = inputs
+        return self.outputs
 
-        # loop through layers to continue forward pass
-        for l in self.layers:
-            # next layer inputs = current layer outputs
-            next_inputs = l(next_inputs)
+class LSTM:
+    def __init__(self, input_size):
+        pass
 
-        # after the loop, next inputs will be the outputs of the output layer
-        return next_inputs
-
-
-    def backward(self, outputs, actual_outputs):
-        # backprpop to output layer manually
-        # each layers input/output shape: (n, 1)
-
-        # grad of the loss func w.r.t the outputs of the last layer before activation function
-        output_n_grad = elementwise_grad(self.loss_function)(outputs, actual_outputs) * self.layers[-1].activation_derivatives
-
-        # gradients of the weighted sums
-        self.layers[-1].neruon_grads = output_n_grad
-
-        # gradients of the weights
-        self.layers[-1].gradients = np.dot(output_n_grad, self.layers[-1].inputs.T)
-
-
-        # loop through layers backwards
-        layer_ind = len(self.layers) - 1
-        # stopping loop at 2nd layer (1st hidden layer) because input layer doesn't have weights and biases to optimize
-        while layer_ind > 0:
-            
-            # gradient of the preactivations of the current layer w.r.t the preactivations in the previous layer  
-            # x = np.dot(self.layers[layer_ind].weights, self.layers[layer_ind - 1].activation_derivatives)
-            x = self.layers[layer_ind].weights * self.layers[layer_ind - 1].activation_derivatives.T
-            # output -> matrix with shape (num_current_neurons, num_previous neurons)
-
-
-            # previous layer neuron gradients = x * current layer neuron gradients
-            # output -> matrix with shape (num_prev_neruons, 1)
-            n_grads = np.dot(x.T , self.layers[layer_ind].neuron_grads)
-
-            
-            self.layers[-1].neuron_gradients = n_grads
-
-            # previous layer weight gradients = previous layer inputs * previous layer neuron gradients
-            self.layers[layer_ind - 1].gradients = np.dot(n_grads, self.layers[layer_ind - 1].inputs.T)
-        
-            layer_ind -= 1
-        
-    def gradient_descent(self, learning_rate):
-        for l in self.layers:
-            l.weights -= (l.gradients * learning_rate)
-                
-
-
-    def train(self, x, y, learning_rate, epochs):
-        for _ in range(epochs):
-            x_train, y_train = x[: round(len(x) * 0.70)], y[: round(len(y) * 0.70)]
-            x_test, y_test = x[round(len(x) * 0.70):], y[round(len(y) * 0.70):]
-            
-            for input, output in zip(x_train, y_train):
-                pred = self.__call__(input)
-                self.backward(pred, output)
-                self.gradient_descent(learning_rate=learning_rate)
-            
-            loss = []
-            for input, output in zip(x_test, y_test):
-                pred = self.__call__(input)
-                loss.append(self.loss_function(pred, output))
-
-            print('Loss:' + str(np.average(np.array(loss))))
-
-
-        
-     
-
-
-
-
-# mock dataset
-dataset = {
-    'X': [[[i]] for i in range(100)],
-    'Y': [[[-7.55]], [[1.7]], [[0.46]], [[12.87]], [[3.23]], [[9.0]], [[13.76]], [[5.5]], [[8.81]], [[10.42]], [[9.54]], [[15.16]], [[2.82]], [[15.1]], [[9.01]], [[24.64]], [[7.73]], [[10.26]], [[21.15]], [[25.27]], [[22.38]], [[13.04]], [[26.19]], [[16.25]], [[29.1]], [[20.3]], [[28.7]], [[35.27]], [[26.09]], [[35.0]], [[39.65]], [[31.43]], [[37.67]], [[42.35]], [[35.52]], [[32.1]], [[32.9]], [[37.37]], [[36.36]], [[43.16]], [[43.46]], [[36.63]], [[51.89]], [[37.38]], [[47.2]], [[43.84]], [[50.8]], [[53.6]], [[50.64]], [[39.8]], [[48.79]], [[52.59]], [[56.62]], [[56.07]], [[51.17]], [[48.43]], [[62.9]], [[65.31]], [[67.9]], [[60.12]], [[52.94]], [[64.39]], [[60.18]], [[55.91]], [[69.97]], [[62.98]], [[59.4]], [[59.85]], [[76.19]], [[77.52]], [[65.18]], [[78.6]], [[73.91]], [[67.58]], [[77.51]], [[68.71]], [[82.41]], [[79.29]], [[80.03]], [[72.38]], [[75.47]], [[71.64]], [[85.3]], [[87.85]], [[84.11]], [[82.8]], [[90.73]], [[88.83]], [[92.51]], [[91.63]], [[82.8]], [[81.69]], [[89.7]], [[85.67]], [[90.61]], [[99.3]], [[96.85]], [[100.66]], [[107.01]], [[102.59]]]
-}
-
-
-# linear activation function
-# x: matrix of shape (n, 1)
-def linear_activation(x):
-    return x
-
-
-# MSE loss function
-# inputs are of shape (n, 1)
-def mean_squared_error_loss(inputs, actuals):
-    mse = (inputs - actuals) ** 2
-    # mean along the rows outputs an array of shape (1, )
-    mse = mse.mean(0)
-    return mse[0]
-
-mlp = MultilayerPerceptron(num_inputs=1, loss_function=mean_squared_error_loss)
-mlp.add_layer(activation=linear_activation, num_neurons=4)
-mlp.add_layer(activation=linear_activation, num_neurons=5)
-mlp.add_layer(activation=linear_activation, num_neurons=1)
-
-
-pred = mlp([[-1]])
-mlp.train(dataset['X'], dataset['Y'], learning_rate=0.01, epochs=100)
+    def add_layer(self, ):
+        pass
---
+++ premain.py
@@ -0,0 +1,185 @@
+import numpy as np
+from autograd import elementwise_grad
+
+
+
+# function: data handler
+        
+    
+
+class Layer:
+    def __init__(self, activation, num_neurons, num_previous_neurons):
+        # initialize weights
+        weights = np.random.normal(0, 2 / num_previous_neurons, num_neurons * num_previous_neurons)
+
+        # weight structure: 
+        
+        # each row corresponds to the weights of a neuron
+        # each column corresponds to the weights of neurons connected to a neuron in the previous layer
+        self.weights = np.reshape(weights, (num_neurons, num_previous_neurons))
+
+        # initialize biases as 0, one for each neuron
+        self.biases = np.zeros((num_neurons, 1))
+
+        # initialize gradients of each weight as 0
+        self.gradients = np.zeros((num_neurons, num_previous_neurons))
+
+        self.activation = activation
+
+        self.activation_derivatives = np.zeros((num_neurons, 1))
+
+        self.neuron_grads = np.zeros((num_neurons, 1))
+
+        self.inputs = np.zeros((num_neurons, 1))
+
+        
+        # data needed for backpropagation
+    
+    def __call__(self, inputs):
+        self.inputs = np.array(inputs)
+
+        # multiply the weights that connect to each previous neuron with the neurons value
+        weighted_sum = np.dot(self.weights, inputs) + self.biases
+
+        self.preactivations = weighted_sum
+
+        # store the derivatives of the activation function with respect to the weighted sum
+        # weighted sum is in n, 1 shape
+        self.activation_derivatives = elementwise_grad(self.activation)(weighted_sum)
+
+        # run outputs through activation function
+        # weighted sum is in n, 1 shape
+        activations = self.activation(weighted_sum)
+        
+        # print(activations)
+    
+        return activations
+    
+
+
+# class: Optimizer ??
+
+class MultilayerPerceptron:
+    # layer sizes: sizes of the non-input layers
+    # activations: activation fucntions for non-input layers
+    def __init__(self, num_inputs, loss_function):
+        # non-input layers
+        self.layers = []
+        self.loss_function = loss_function
+        
+        self.num_inputs = num_inputs
+
+    def add_layer(self, activation, num_neurons):
+        previous_layer_size = self.num_inputs if len(self.layers) == 0 else len(self.layers[-1].weights)
+        self.layers.append(Layer(activation, num_neurons, previous_layer_size))
+
+    def __call__(self, inputs):
+        # put inputs into the input layer
+        next_inputs = inputs
+
+        # loop through layers to continue forward pass
+        for l in self.layers:
+            # next layer inputs = current layer outputs
+            next_inputs = l(next_inputs)
+
+        # after the loop, next inputs will be the outputs of the output layer
+        return next_inputs
+
+
+    def backward(self, outputs, actual_outputs):
+        # backprpop to output layer manually
+        # each layers input/output shape: (n, 1)
+
+        # grad of the loss func w.r.t the outputs of the last layer before activation function
+        output_n_grad = elementwise_grad(self.loss_function)(outputs, actual_outputs) * self.layers[-1].activation_derivatives
+
+        # gradients of the weighted sums
+        self.layers[-1].neuron_grads = output_n_grad
+
+        # gradients of the weights
+        self.layers[-1].gradients = np.dot(output_n_grad, self.layers[-1].inputs.T)
+
+        # loop through layers backwards
+        layer_ind = len(self.layers) - 1
+        # stopping loop at 2nd layer (1st hidden layer) because input layer doesn't have weights and biases to optimize
+        while layer_ind > 0:
+            # gradient of the preactivations of the current layer w.r.t the preactivations in the previous layer  
+            # output -> matrix with shape (num_previous neurons, num_current_neurons)
+            x = self.layers[layer_ind].weights.T * self.layers[layer_ind - 1].activation_derivatives
+
+
+            # previous layer neuron gradients = x * current layer neuron gradients
+            # output -> matrix with shape (num_prev_neruons, 1)
+            n_grads = np.dot(x , self.layers[layer_ind].neuron_grads)
+            self.layers[-1].neuron_gradients = n_grads
+
+
+            # previous layer weight gradients = previous layer inputs * previous layer neuron gradients
+            self.layers[layer_ind - 1].gradients = np.dot(n_grads, self.layers[layer_ind - 1].inputs.T)
+
+        
+            layer_ind -= 1
+        
+    def gradient_descent(self, learning_rate):
+        for l in self.layers:
+            l.weights -= (l.gradients * learning_rate)
+                
+
+
+    def train(self, x, y, learning_rate, epochs):
+        for _ in range(epochs):
+            x_train, y_train = x[: round(len(x) * 0.70)], y[: round(len(y) * 0.70)]
+            x_test, y_test = x[round(len(x) * 0.70):], y[round(len(y) * 0.70):]
+            
+
+            for input, output in zip(x_train, y_train):
+                pred = self.__call__(input)
+                self.backward(pred, output)
+                self.gradient_descent(learning_rate=learning_rate)
+                
+                
+            
+            continue
+
+            loss = []
+            for input, output in zip(x_test, y_test):
+                pred = self.__call__(input)
+                loss.append(self.loss_function(pred, output))
+
+            # print('Loss:' + str(np.average(np.array(loss))))
+
+
+        
+    
+
+# mock dataset
+dataset = {
+    'X': [[[i]] for i in range(100)],
+    'Y': [[[-7.55]], [[1.7]], [[0.46]], [[12.87]], [[3.23]], [[9.0]], [[13.76]], [[5.5]], [[8.81]], [[10.42]], [[9.54]], [[15.16]], [[2.82]], [[15.1]], [[9.01]], [[24.64]], [[7.73]], [[10.26]], [[21.15]], [[25.27]], [[22.38]], [[13.04]], [[26.19]], [[16.25]], [[29.1]], [[20.3]], [[28.7]], [[35.27]], [[26.09]], [[35.0]], [[39.65]], [[31.43]], [[37.67]], [[42.35]], [[35.52]], [[32.1]], [[32.9]], [[37.37]], [[36.36]], [[43.16]], [[43.46]], [[36.63]], [[51.89]], [[37.38]], [[47.2]], [[43.84]], [[50.8]], [[53.6]], [[50.64]], [[39.8]], [[48.79]], [[52.59]], [[56.62]], [[56.07]], [[51.17]], [[48.43]], [[62.9]], [[65.31]], [[67.9]], [[60.12]], [[52.94]], [[64.39]], [[60.18]], [[55.91]], [[69.97]], [[62.98]], [[59.4]], [[59.85]], [[76.19]], [[77.52]], [[65.18]], [[78.6]], [[73.91]], [[67.58]], [[77.51]], [[68.71]], [[82.41]], [[79.29]], [[80.03]], [[72.38]], [[75.47]], [[71.64]], [[85.3]], [[87.85]], [[84.11]], [[82.8]], [[90.73]], [[88.83]], [[92.51]], [[91.63]], [[82.8]], [[81.69]], [[89.7]], [[85.67]], [[90.61]], [[99.3]], [[96.85]], [[100.66]], [[107.01]], [[102.59]]]
+}
+
+
+# linear activation function
+# x: matrix of shape (n, 1)
+def linear_activation(x):
+    return x
+
+
+# MSE loss function
+# inputs are of shape (n, 1)
+def mean_squared_error_loss(inputs, actuals):
+    mse = (inputs - actuals) ** 2
+    # mean along the rows outputs an array of shape (1, )
+    mse = mse.mean(0)
+    return mse[0]
+
+mlp = MultilayerPerceptron(num_inputs=1, loss_function=mean_squared_error_loss)
+mlp.add_layer(activation=linear_activation, num_neurons=4)
+mlp.add_layer(activation=linear_activation, num_neurons=5)
+mlp.add_layer(activation=linear_activation, num_neurons=1)
+
+
+# pred = mlp([[-1]])
+# mlp.backward(pred, [[-2]])
+# quit()
+mlp.train(dataset['X'], dataset['Y'], learning_rate=0.01, epochs=50)
